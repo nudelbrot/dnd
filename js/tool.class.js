f0ec0f47f50e397eb686bd4c3e99b8aa681ebe8d
Gutrund <friedrich-alexander@gmx.de>
2017-05-14 17:13:28 +0200

    buckettool is now fucking fast

--- js/tool.class.js
+++ js/tool.class.js
@@ -5,7 +5,6 @@ class Tool {
         this.button = undefined;
         this.panel = $("<div class='panel panel-default toolSettings'></div>");
         this.panel.append($("<div class='panel-body'></div>"));
-        this.cursor = {id: "default", dx: 0, dy: 0};
     }
     onMouseMove(self, evt) {
     }
@@ -18,24 +17,6 @@ class Tool {
 
     evtToCoordinates(evt) {
         return { x: (evt.offsetX - this.map.translation.x) / this.map.cellWidth, y: (evt.offsetY - this.map.translation.y) / this.map.cellHeight };
-    }
-
-    enableCursor(){
-        var canvas = $("<canvas></canvas>")[0];
-        canvas.width = 24;
-        canvas.height = 24;
-        var ctx = canvas.getContext("2d");
-        ctx.font = "26px Material Icons";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.moveTo(0,0);
-        ctx.fillStyle = "#ffffff";
-        ctx.fillText(this.cursor.id, 12,12);
-        ctx.fillStyle = "#000000";
-        ctx.font = "24px Material Icons";
-        ctx.fillText(this.cursor.id, 12,12);
-        var dataURL = canvas.toDataURL('image/png')
-        $(this.map.canvas).css('cursor', 'url('+dataURL+') ' + this.cursor.dx + ' ' + this.cursor.dy + ', auto');
     }
 
 }
@@ -51,7 +32,6 @@ class RectSelectionTool extends SelectionTool {
     constructor(map) {
         super(map);
         this.icon = "crop_free";
-        this.cursor.id = this.icon;
         this.button = $('<button type="button" class="btn btn-default"> <span class="material-icons">' + this.icon + '</span></button>');
     }
 }
@@ -60,7 +40,6 @@ class MagicStickTool extends SelectionTool {
     constructor(map) {
         super(map);
         this.icon = "open_with";
-        this.cursor = {id: "open_with", dx: 10, dy: 10};
         this.button = $('<button type="button" class="btn btn-default"> <span class="material-icons">' + this.icon + '</span></button>');
         this.Stack = [];
         this.cellsToFill = [];
@@ -180,10 +159,12 @@ class SculptureTool extends Tool {
         var body = this.panel.find(".panel-body");
     }
 
-    changeCellFillstyle(x, y, fillStyle){
+    changeCellFillstyle(x, y, fillStyle, render=true){
       if (this.map.fillStyle == fillStyle){
           this.map.removeCell(x, y);
-          this.map.render();
+          if (render){
+              this.map.render();
+          }
           return false;
       } else {
           return this.map.changeCellFillstyle(x, y, fillStyle)
@@ -212,54 +193,11 @@ class SculptureTool extends Tool {
 
 }
 
-class PathTool extends SculptureTool {
-    constructor(map, foregroundColorPicker, backgroundColorPicker) {
-        super(map, foregroundColorPicker, backgroundColorPicker);
-        this.icon = "linear_scale";
-        this.button = $('<button type="button" class="btn btn-default"> <span class="material-icons">' + this.icon + '</span></button>');
-        this.positions = [];
-    }
-  onMouseDown(evt){
-    super.onClick(evt);
-    var fillStyle = evt.which == 1 ? this.foregroundColor : this.backgroundColor;
-    var pos = this.evtToCoordinates(evt);
-    pos.x = Math.floor(pos.x);
-    pos.y = Math.floor(pos.y);
-    this.positions.push(pos);
-    if(this.positions.length == 2){
-      if(evt.shiftKey){
-        this.line(this.positions[0].x, this.positions[0].y, this.positions[1].x, this.positions[1].y, fillStyle);
-        this.positions.splice(0,1);
-      }else{
-        this.line(this.positions[0].x, this.positions[0].y, this.positions[1].x, this.positions[1].y, fillStyle);
-        this.positions = [];
-      }
-    }
-  }
-  line(x0, y0, x1, y1, fillStyle){
-    var dx =  Math.abs(x1-x0);
-    var sx = x0<x1 ? 1 : -1;
-    var dy = -Math.abs(y1-y0)
-    var sy = y0<y1 ? 1 : -1;
-    var err = dx+dy, e2;
-    var i = 0;
-
-    while(1 && i++ < 200){
-      this.changeCellFillstyle(x0, y0, fillStyle);
-      if (x0==x1 && y0==y1) break;
-      e2 = 2*err;
-      if (e2 > dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
-      if (e2 < dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
-    }
-  }
-}
-
 class PencilTool extends SculptureTool {
     constructor(map, foregroundColorPicker, backgroundColorPicker) {
         super(map, foregroundColorPicker, backgroundColorPicker);
         this.size = 1;
         this.icon = "edit"
-        this.cursor = {id: "edit", dx: 2, dy: 20};
         this.button = $('<button type="button" class="btn btn-default"> <span class="material-icons">' + this.icon + '</span></button>');
         this.mouseDown = false;
         this.latest = { x: 0, y: 0 };
@@ -278,21 +216,11 @@ class PencilTool extends SculptureTool {
       if(cell) cell.render();
     }
   }
-    onMouseDown(evt){
-      if(evt.which == 1 || evt.which == 3){
-        this.mouseDown = true;
-      }
-    }
-
-    onMouseUp(evt){
-      this.mouseDown = false;
-    }
-
     onMouseMove(evt) {
         if (!evt.shiftKey) {
             this.shifted.reset();
         }
-        if (this.mouseDown && (evt.which == 1 || evt.which == 3)) {
+        if (evt.which == 1 || evt.which == 3) {
             var fillStyle = evt.which == 1 ? this.foregroundColor : this.backgroundColor;
             var pos = this.evtToCoordinates(evt);
             pos.x = Math.floor(pos.x);
@@ -300,15 +228,16 @@ class PencilTool extends SculptureTool {
 
             if (evt.shiftKey) {
                 if (this.shifted.direction == "vertical") {
-                  this.changeCellFillstyle(pos.x, this.shifted.y, fillStyle);
+                  this.changeColor(pos.x, this.shifted.y, fillStyle);
+                  this.map.getCell(pos.x, this.shifted.y).render();
                 } else if (this.shifted.direction == "horizontal") {
-                  this.changeCellFillstyle(this.shifted.x, pos.y, fillStyle);
+                  this.changeColor(this.shifted.x, pos.y, fillStyle);
+                  this.map.getCell(this.shifted.x, pos.y).render();
                 } else {
-                    if (this.shifted.x || this.shifted.y) {
+                    if (this.shifted.x) {
                         if (Math.abs(this.shifted.x - pos.x) >= 1) {
                             this.shifted.direction = "vertical";
-                        }
-                        if (Math.abs(this.shifted.y - pos.y) >= 1) {
+                        } else if (Math.abs(this.shifted.y - pos.y) >= 1) {
                             this.shifted.direction = "horizontal";
                         }
                     } else {
@@ -337,7 +266,6 @@ class BucketTool extends SculptureTool {
     constructor(map, foregroundColorPicker, backgroundColorPicker) {
         super(map, foregroundColorPicker, backgroundColorPicker);
         this.icon = "format_color_fill";
-        this.cursor.id = this.icon;
         this.button = $('<button type="button" class="btn btn-default"> <span class="material-icons">' + this.icon + '</span></button>');
         this.Stack = [];
         this.cellsToFill = [];
@@ -348,26 +276,24 @@ class BucketTool extends SculptureTool {
         this.xmax
         this.ymin
         this.ymax
+        this.abort = false;
     }
     onClick(evt) {
         super.onClick(evt);
         var pos = this.evtToCoordinates(evt);
-        pos = [Math.floor(pos.x), Math.floor(pos.y)];
-
-        var coordString = pos[0] + "/" + pos[1]
-        this.listOfCurrentCells = Object.keys(this.map.data)
-        this.inListOfCurrentCells = $.inArray(coordString, this.listOfCurrentCells)
-        var xAndYofCurrentCells = this.listOfCurrentCells.map(function (obj) {
-            var coord = obj.split("/")
-            //console.debug(coord)
-            return [coord[0], coord[1]]
+        var x = Math.floor(pos.x)
+        var y = Math.floor(pos.y)
+        this.listOfCurrentCells = Object.values(this.map.data)
+        this.inListOfCurrentCells = this.map.isCell(x,y)
+        this.coordsOfCurrentCells = this.listOfCurrentCells.map(function (obj) {
+            return [obj.x, obj.y]
         });
-        var xOfCurrentCells = xAndYofCurrentCells.map(function (obj) {
+        var xOfCurrentCells = this.coordsOfCurrentCells.map(function (obj) {
             return obj[0]
-        })
-        var yOfCurrentCells = xAndYofCurrentCells.map(function (obj) {
+        });
+        var yOfCurrentCells = this.listOfCurrentCells.map(function (obj) {
             return obj[1]
-        })
+        });
         this.xmin = Math.min.apply(null, xOfCurrentCells)
         this.xmax = Math.max.apply(null, xOfCurrentCells)
         this.ymin = Math.min.apply(null, yOfCurrentCells)
@@ -379,27 +305,42 @@ class BucketTool extends SculptureTool {
         } else {
             newColor = this.backgroundColor;
         }
-        if (this.outside(pos[0], pos[1])) {
+        //console.debug("x/y: " + x + "/" + y + "\nxMin/xMax: " + this.xmin + "/" + this.xmax + "\nyMin/yMax: " + this.ymin + "/" + this.ymax)
+        if (!this.outside(x, y)) {
+            if (this.map.isCell(x,y)){
+                this.old = this.map.getCell(x, y).fillStyle;
+                if (newColor == this.old){
+                    return;
+                }
+            } else {
+                this.old = this.map.fillStyle;
+            }
+            this.cellsToFill = [];
+            this.Stack = [];
+            //console.debug("Start flooding")
+            this.floodFill(x, y);
+            //console.debug("Ended flooding: ", this.cellsToFill);
+            if (!this.abort){
+                var newColorIsBackgroundColor = (newColor == this.map.fillStyle)
+                for (var i = 0; i < this.cellsToFill.length; i++) {
+                    this.changeCellFillstyle(this.cellsToFill[i][0], this.cellsToFill[i][1], newColor, !newColorIsBackgroundColor);
+                }
+                if (newColorIsBackgroundColor) {
+                    this.map.render();
+                }
+            } else {
+                this.map.fillStyle = newColor;
+                this.map.render();
+            }
+        } else {
             this.map.fillStyle = newColor;
             this.map.render();
-            return;
         }
-        if (this.map.isCell(pos[0], pos[1]) && newColor == this.map.getCell(pos[0], pos[1]).fillStyle) {
-            return;
-        }
-        this.old = this.map.getCell(pos[0], pos[1]).fillStyle;
-        
-        this.cellsToFill = [];
-        this.Stack = [];
-        //console.debug("Start flooding")
-        this.floodFill(pos[0], pos[1]);
-        //console.debug("Ended flooding: ", this.cellsToFill);
-        for (var i = 0; i < this.cellsToFill.length; i++) {
-            this.changeCellFillstyle(this.cellsToFill[i][0], this.cellsToFill[i][1], newColor);
-        }
+        //console.debug("ABORT: " + this.abort);
+        this.abort = false;
     }
     outside(x, y) {
-        //console.debug("x: " + x + "\nxmin: " + this.xmin + "\nxmax: " + this.xmax + "\nymin: " + this.ymin + "\nymax: " + this.ymax)
+        //console.debug("x/y: " + x + "/" + y + "\nxMin/xMax: " + this.xmin + "/" + this.xmax + "\nyMin/yMax: " + this.ymin + "/" + this.ymax)
         if (x < this.xmin || x > this.xmax || y < this.ymin || y > this.ymax) {
             return true;
         } else {
@@ -429,15 +370,18 @@ class BucketTool extends SculptureTool {
     }
 
     alreadyFilled(x, y) {
-        if (!this.outside(x, y)) {
-            var coordString = x + "/" + y;
-            var cellsToFillStringArray = this.cellsToFill.map(function (obj) { return obj[0] + "/" + obj[1] });
-            if ($.inArray(coordString, this.listOfCurrentCells) != -1) {
-                return ((this.map.getCell(x, y).fillStyle != this.old) || $.inArray(coordString, cellsToFillStringArray) != -1);
+        //console.debug(x +"/"+ y + " innerhalb: " + !this.outside(x,y) + " KlickAufZelle: " + this.inListOfCurrentCells)
+        if ((!this.outside(x,y) || this.inListOfCurrentCells) && !this.abort){
+            var coord = [x,y];
+            var inCellsToFill = this.cellsToFill.contains(coord);
+            if (this.coordsOfCurrentCells.contains(coord)) {
+                return ((this.map.getCell(x, y).fillStyle != this.old) || inCellsToFill);
             } else {
-                return ($.inArray(coordString, cellsToFillStringArray) != -1 || this.inListOfCurrentCells != -1);
+                return (inCellsToFill || this.inListOfCurrentCells);
             }
         } else {
+            this.Stack = []
+            this.abort = true;
             return true;
         }
     }
@@ -453,7 +397,6 @@ class Toolbar {
         this.backgroundColor = "#ffffff";
         this.addColorpickers();
         this.addTools();
-        this.customCursor = false;
         this.backgroundColorPicker.colorpicker("setValue", "#ffffff");
         target.prepend(this.panel[0]);
         var t = this;
@@ -472,21 +415,15 @@ class Toolbar {
         this.rectSelector = new RectSelectionTool(this.map);
         this.magicStick = new MagicStickTool(this.map);
         this.pencil = new PencilTool(this.map, this.foregroundColorPicker, this.backgroundColorPicker);
-        this.bucket = new BucketTool(this.map, this.foregroundColorPicker, this.backgroundColorPicker);
-        this.path = new PathTool(this.map, this.foregroundColorPicker, this.backgroundColorPicker);
-
+        this.bucket = new BucketTool(this.map, this.foregroundColorPicker, this.backgroundColorPicker); 
         var grp = $('<div class="btn-group" role="group"></div>');
 
         grp.append(this.rectSelector.button);
         grp.append(this.magicStick.button);
         grp.append(this.pencil.button);
-        grp.append(this.path.button);
         grp.append(this.bucket.button);
         var t = this;
 
-        this.path.button.on("click", function () {
-            t.setActiveTool(t.path);
-        });
         this.rectSelector.button.on("click", function () {
             t.setActiveTool(t.rectSelector);
         });
@@ -501,7 +438,7 @@ class Toolbar {
         });
 
         this.activeTool = this.pencil;
-        this.setActiveTool(this.pencil);
+        //this.setActiveTool(this.pencil);
         var body = this.panel.find("#tb_body");
         body.append(grp);
 
@@ -523,7 +460,6 @@ class Toolbar {
                 t.foregroundColor = e.value;
                 t.pencil.foregroundColor = e.value;
                 t.bucket.foregroundColor = e.value;
-                t.path.foregroundColor = e.value;
             }
         });
         this.backgroundColorPicker.colorpicker().on("changeColor", function (e) {
@@ -531,7 +467,6 @@ class Toolbar {
                 t.backgroundColor = e.value;
                 t.pencil.backgroundColor = e.value;
                 t.bucket.backgroundColor = e.value;
-                t.path.backgroundColor = e.value;
             }
         });
         body.append(this.foregroundColorPicker);
@@ -544,11 +479,6 @@ class Toolbar {
         this.panel.find(".btn").removeClass("btn-primary");
         tool.button.addClass("btn-primary");
         this.panel.find(".panel-body").append(this.activeTool.panel);
-        if(this.customCursor){
-          this.activeTool.enableCursor();
-        }else{
-          $("body").css('cursor', 'default');
-        }
     }
 
     onMouseMove(evt) {
